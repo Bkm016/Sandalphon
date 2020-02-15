ed0aa101d018a4f3d97a333ede5a4bce415f7734
sky <sky@asgard.me>
2020-02-15 11:13:38 +0800

    Initial commit

---
+++ src/main/java/ink/ptms/sandalphon/Sandalphon.java
@@ -0,0 +1,17 @@
+package ink.ptms.sandalphon;
+
+import io.izzel.taboolib.loader.Plugin;
+
+@Plugin.Version(5.17)
+public final class Sandalphon extends Plugin {
+
+    @Override
+    public void onStarting() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onStopping() {
+        // Plugin shutdown logic
+    }
+}
