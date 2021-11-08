10e8f8674d3d652cf2aa47c1f042484a5ac5e7bd
坏黑 <b@skymc.me>
2021-11-08 16:52:31 +0800

    Update TabooLib

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ taboolib {
     install("module-kether", "expansion-command-helper", "expansion-player-database")
     install("platform-bukkit")
     classifier = null
-    version = "6.0.3-23"
+    version = "6.0.4-3"
 }
 
 repositories {
